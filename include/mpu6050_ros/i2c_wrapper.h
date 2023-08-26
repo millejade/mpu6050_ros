1f0940b4a26c48e243063a05c78f8d8ab6f79658
millejade <jadecabrera.jc@gmail.com>
2023-08-26 09:08:17 +0000

    Initial directories and files needed.

---
+++ include/mpu6050_ros/i2c_wrapper.h
@@ -0,0 +1,13 @@
+// Copyright here
+
+#ifndef I2C_WRAPPER_H
+#define I2C_WRAPPER_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+
+#endif
