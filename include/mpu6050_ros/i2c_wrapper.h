4e1ed6fb139dac843cab28d46c03f67ff55eb8c6
Millejade Cabrera <Millejade.Cabrera@analog.com>
2023-08-27 16:51:29 +0800

    Added initial class for I2CWrapper

--- include/mpu6050_ros/i2c_wrapper.h
+++ include/mpu6050_ros/i2c_wrapper.h
@@ -9,5 +9,25 @@
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 
+class I2CWrapper
+{
+  public:
+    I2CWrapper();
+
+    ~I2CWrapper();
+
+    uint8_t initI2C();
+
+    uint8_t deinitI2C();
+
+    uint8_t isDataAvailable();
+
+    uint8_t readData();
+
+    uint8_t writeData();
+
+  private:
+
+}
 
 #endif
